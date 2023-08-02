d6379781a5150f7ed570a310d90633479ee89c1d
hakuamesan <53928035+hakuamesan@users.noreply.github.com>
2023-08-02 20:05:58 +0100

    removed esp8266

--- examples/simple_server/simple_server.ino
+++ examples/simple_server/simple_server.ino
@@ -6,13 +6,8 @@
 //
 
 #include <Arduino.h>
-#ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#endif
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
