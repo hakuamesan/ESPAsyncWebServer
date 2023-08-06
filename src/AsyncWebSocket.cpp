8d66168884a6470d651771024fa49c735f476b9c
Hakuame <hakuame@protonmail.com>
2023-08-06 10:02:55 +0100

    removed esp8266

--- src/AsyncWebSocket.cpp
+++ src/AsyncWebSocket.cpp
@@ -724,37 +724,6 @@ size_t AsyncWebSocketClient::printf(const char *format, ...) {
   return len;
 }
 
-#ifndef ESP32
-size_t AsyncWebSocketClient::printf_P(PGM_P formatP, ...) {
-  va_list arg;
-  va_start(arg, formatP);
-  char* temp = new char[MAX_PRINTF_LEN];
-  if(!temp){
-    va_end(arg);
-    return 0;
-  }
-  char* buffer = temp;
-  size_t len = vsnprintf_P(temp, MAX_PRINTF_LEN, formatP, arg);
-  va_end(arg);
-
-  if (len > (MAX_PRINTF_LEN - 1)) {
-    buffer = new char[len + 1];
-    if (!buffer) {
-   	  delete[] temp;
-      return 0;
-    }
-    va_start(arg, formatP);
-    vsnprintf_P(buffer, len + 1, formatP, arg);
-    va_end(arg);
-  }
-  text(buffer, len);
-  if (buffer != temp) {
-    delete[] buffer;
-  }
-  delete[] temp;
-  return len;
-}
-#endif
 
 void AsyncWebSocketClient::text(const char * message, size_t len){
   _queueMessage(new AsyncWebSocketBasicMessage(message, len));
@@ -1032,19 +1001,6 @@ size_t AsyncWebSocket::printfAll(const char *format, ...) {
   return len;
 }
 
-#ifndef ESP32
-size_t AsyncWebSocket::printf_P(uint32_t id, PGM_P formatP, ...){
-  AsyncWebSocketClient * c = client(id);
-  if(c != NULL){
-    va_list arg;
-    va_start(arg, formatP);
-    size_t len = c->printf_P(formatP, arg);
-    va_end(arg);
-    return len;
-  }
-  return 0;
-}
-#endif
 
 size_t AsyncWebSocket::printfAll_P(PGM_P formatP, ...) {
   va_list arg;
@@ -1249,10 +1205,8 @@ AsyncWebSocketResponse::AsyncWebSocketResponse(const String& key, AsyncWebSocket
     _state = RESPONSE_FAILED;
     return;
   }
-#ifdef ESP8266
-  sha1(key + WS_STR_UUID, hash);
-#else
-  (String&)key += WS_STR_UUID;
+
+  (String&) key += WS_STR_UUID;
   mbedtls_sha1_context ctx;
   mbedtls_sha1_init(&ctx);
 #if MBEDTLS_VERSION_NUMBER >= 0x02070000 && MBEDTLS_VERSION_NUMBER < 0x03000000
@@ -1265,7 +1219,6 @@ AsyncWebSocketResponse::AsyncWebSocketResponse(const String& key, AsyncWebSocket
   mbedtls_sha1_finish(&ctx, hash);
 #endif
   mbedtls_sha1_free(&ctx);
-#endif
   base64_encodestate _state;
   base64_init_encodestate(&_state);
   int len = base64_encode_block((const char *) hash, 20, buffer, &_state);
